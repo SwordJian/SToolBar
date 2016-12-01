113a0a7e5794617748236b8585212ea1edac688a
SwordJian <601827306@qq.com>
2016-12-01 17:21:24 +0800

    修改了SSStatus，支持华为5.X通知栏

---
+++ stoolbar/src/main/java/com/github/swordjian/stoolbar/BuildProperties.java
@@ -0,0 +1,86 @@
+package com.github.swordjian.stoolbar;
+
+import android.os.Environment;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Created by SwordJian on 2016/12/1.
+ *
+ * @author SwordJian
+ * @editor SwordJian on 2016/12/1
+ */
+
+public class BuildProperties {
+
+    private final Properties properties;
+
+    private BuildProperties() throws IOException {
+        properties = new Properties();
+        properties.load(new FileInputStream(new File(Environment.getRootDirectory(), "build.prop")));
+    }
+
+    public boolean containsKey(final Object key) {
+        return properties.containsKey(key);
+    }
+
+    public boolean containsValue(final Object value) {
+        return properties.containsValue(value);
+    }
+
+    public Set<Map.Entry<Object, Object>> entrySet() {
+        return properties.entrySet();
+    }
+
+    public String getProperty(final String name) {
+        return properties.getProperty(name);
+    }
+
+    public String getProperty(final String name, final String defaultValue) {
+        return properties.getProperty(name, defaultValue);
+    }
+
+    public boolean isEmpty() {
+        return properties.isEmpty();
+    }
+
+    public Enumeration<Object> keys() {
+        return properties.keys();
+    }
+
+    public Set<Object> keySet() {
+        return properties.keySet();
+    }
+
+    public int size() {
+        return properties.size();
+    }
+
+    public Collection<Object> values() {
+        return properties.values();
+    }
+
+    public static BuildProperties newInstance() throws IOException {
+        return new BuildProperties();
+    }
+
+    private static final String KEY_EMUI_VERSION_CODE = "ro.build.hw_emui_api_level";
+
+    public static boolean isEMUI() {
+        try {
+            //BuildProperties 是一个工具类，下面会给出代码
+            final BuildProperties prop = BuildProperties.newInstance();
+            return prop.getProperty(KEY_EMUI_VERSION_CODE, null) != null;
+        } catch (final IOException e) {
+            return false;
+        }
+    }
+
+}
--- stoolbar/src/main/java/com/github/swordjian/stoolbar/SStatusBar/SStatusBar.java
+++ stoolbar/src/main/java/com/github/swordjian/stoolbar/SStatusBar/SStatusBar.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import static android.os.Build.HOST;
+
 /**
  * @author SwordJian
  */
@@ -16,9 +18,9 @@ public class SStatusBar {
 
     private static SStatusBar mInstance;
 
-    public static SStatusBar getInstance(){
-        if (mInstance == null){
-            synchronized (SStatusBar.class){
+    public static SStatusBar getInstance() {
+        if (mInstance == null) {
+            synchronized (SStatusBar.class) {
                 mInstance = new SStatusBar();
             }
         }
@@ -27,22 +29,104 @@ public class SStatusBar {
 
     @TargetApi(21)
     public void setTranslucentStatus(Activity act) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = act.getWindow();
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            Window win = act.getWindow();
-            WindowManager.LayoutParams winParams = win.getAttributes();
-            final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
-            winParams.flags |= bits;
-            win.setAttributes(winParams);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                if (Build.MANUFACTURER.equalsIgnoreCase("HUAWEI"))
+                {
+                    setTranslucentStatusAPI19(act);
+                } else {
+                    setTranslucentStatusAPI21(act);
+                }
+            } else {
+                setTranslucentStatusAPI19(act);
+            }
         }
     }
+
+
+//主板:PLK-TL01H
+//    系统启动程序版本号:unknown
+//    系统定制商:HONOR
+//    cpu指令集:arm64-v8a
+//            cpu指令集2
+//    设置参数:HWPLK
+//    显示屏参数:PLK-TL01HC01B211
+//    硬件识别码:HONOR/PLK-TL01H/HWPLK:5.0.2/HONORPLK-TL01H/C01B211:user/release-keys
+//    硬件名称:hi3635
+//    HOST:huawei-desktop
+//    修订版本列表:HONORPLK-TL01H
+//    硬件制造商:HUAWEI
+//    版本:PLK-TL01H
+//    硬件序列号:A2JDU16429009693
+//    手机制造商:PLK-TL01H
+//    描述Build的标签:release-keys
+//    TIME:1460804799000
+//    builder类型:user
+//    USER:huawei
+
+    private String getDeviceInfo() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("主板:" + Build.BOARD+ "\n");
+        sb.append(
+                "系统启动程序版本号:" + Build.BOOTLOADER+ "\n");
+        sb.append(
+                "系统定制商:" + Build.BRAND+ "\n");
+        sb.append(
+                "cpu指令集:" + Build.CPU_ABI+ "\n");
+        sb.append(
+                "cpu指令集2" + Build.CPU_ABI2+ "\n");
+        sb.append(
+                "设置参数:" + Build.DEVICE+ "\n");
+        sb.append(
+                "显示屏参数:" + Build.DISPLAY+ "\n"); //mk_huashan-userdebug 7.1 NDE63X 55d35c7b3b test-keys
+//        sb.append(
+//                "无线电固件版本:" + Build.getRadioVersion());
+        sb.append(
+                "硬件识别码:" + Build.FINGERPRINT+ "\n");
+        sb.append(
+                "硬件名称:" + Build.HARDWARE+ "\n");
+        sb.append(
+                "HOST:" + HOST+ "\n");
+        sb.append(
+                "修订版本列表:" + Build.ID+ "\n");
+        sb.append(
+                "硬件制造商:" + Build.MANUFACTURER + "\n");
+        sb.append(
+                "版本:" + Build.MODEL+ "\n");
+        sb.append(
+                "硬件序列号:" + Build.SERIAL+ "\n");
+        sb.append(
+                "手机制造商:" + Build.PRODUCT+ "\n");
+        sb.append(
+                "描述Build的标签:" + Build.TAGS+ "\n");
+        sb.append(
+                "TIME:" + Build.TIME+ "\n");
+        sb.append(
+                "builder类型:" + Build.TYPE+ "\n");
+        sb.append(
+                "USER:" + Build.USER+ "\n");
+        return sb.toString();
+    }
+
+    @TargetApi(21)
+    public void setTranslucentStatusAPI21(Activity act) {
+        Window window = act.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(Color.TRANSPARENT);
+    }
+
+    public void setTranslucentStatusAPI19(Activity act) {
+        Window window = act.getWindow();
+        WindowManager.LayoutParams winParams = window.getAttributes();
+        final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        winParams.flags |= bits;
+        window.setAttributes(winParams);
+    }
+
     @TargetApi(21)
     public void setTranslucentStatus(Activity act, View status_bar) {
         setTranslucentStatus(act);
